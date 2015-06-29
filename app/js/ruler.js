a4926ade9c1316e679ec53cd520509d5dd7d9ac6
maor.frankel <maor.frankel@sizmek.com>
2015-06-29 11:31:14 +0300

    UI fixes

--- app/js/ruler.js
+++ app/js/ruler.js
@@ -257,3 +257,337 @@ var ruler = (function (){
 })();
 
 
+
+/**
+ * Created by maor.frankel on 5/23/15.
+ */
+ruler.rulerConstructor =  function(_canvas, options, rulDimension)
+{
+
+	var canvas = _canvas,
+		context = canvas.getContext('2d'),
+		rulThickness = 0,
+		rulLength = 0,
+		rulScale = 1,
+		dimension = rulDimension || ruler.HORIZONTAL,
+		orgPos = 0,
+		tracker = document.createElement('div');
+
+	var getLength = function (){
+		return rulLength;
+	};
+
+	var getThickness = function(){
+		return rulThickness;
+	};
+
+	var getScale = function(){
+		return rulScale;
+	};
+
+	var setScale = function(newScale){
+		rulScale = parseFloat(newScale);
+		drawPoints();
+		return rulScale;
+	};
+
+	var drawRuler = function (_rulerLength, _rulerThickness, _rulerScale){
+		rulLength = canvas.width = _rulerLength * 4;
+		rulThickness = canvas.height = _rulerThickness;
+		rulScale = _rulerScale || rulScale;
+		context.strokeStyle = options.strokeStyle;
+		context.font = options.fontSize + ' ' + options.fontFamily;
+		context.lineWidth = options.lineWidth;
+		context.beginPath();
+		drawPoints();
+		context.stroke();
+	};
+
+	var drawPoints = function () {
+		var  pointLength = 0,
+			label = '',
+			delta = 0,
+			draw = false,
+			lineLengthMax = 0,
+			lineLengthMed = rulThickness / 2,
+			lineLengthMin = rulThickness / 2;
+
+		for (var pos = 0; pos <= rulLength; pos += 1) {
+			delta = ((rulLength / 2) - pos);
+			draw = false;
+			label = '';
+
+			if (delta % 50 === 0) {
+				pointLength = lineLengthMax;
+				label = Math.round(Math.abs(delta)*rulScale);
+				draw = true;
+			}
+			else if (delta % 25 === 0) {
+				pointLength = lineLengthMed;
+				draw = true;
+			}
+			else if (delta % 5 === 0) {
+				pointLength = lineLengthMin;
+				draw = true;
+			}
+			if (draw) {
+				context.moveTo(pos + 0.5, rulThickness + 0.5);
+				context.lineTo(pos + 0.5, pointLength +  0.5);
+				context.fillText(label, pos + 1.5, (rulThickness / 2) + 1);
+			}
+		}
+	};
+
+	var initTracker = function(){
+		tracker = options.container.appendChild(tracker);
+		ruler.utils.addClasss(tracker, 'rul_tracker');
+		var height = ruler.utils.pixelize(options.rulerHeight);
+		if(dimension === ruler.HORIZONTAL){
+			tracker.style.height = height;
+		}
+		else{
+			tracker.style.width = height;
+		}
+
+		options.container.addEventListener('mousemove', function(e){
+			var posX = e.clientX;
+			var posY = e.clientY;
+			if(dimension === ruler.HORIZONTAL){
+				tracker.style.left = ruler.utils.pixelize(posX - parseInt(options.container.offsetLeft));
+			}
+			else{
+				tracker.style.top = ruler.utils.pixelize(posY - parseInt(options.container.offsetTop)) ;
+			}
+		});
+	};
+	if(options.enableMouseTracking){
+		initTracker();
+	}
+
+
+	return{
+		getLength: getLength,
+		getThickness: getThickness,
+		getScale: getScale,
+		setScale: setScale,
+		dimension: dimension,
+		orgPos: orgPos,
+		canvas: canvas,
+		context: context,
+		drawRuler: drawRuler,
+		drawPoints: drawPoints
+	}
+};
+
+
+
+/**
+ * Created by maor.frankel on 5/23/15.
+ */
+ruler.guideLine = function(line, _dragContainer, lineDimension, options,  curDelta, moveCB, event){
+
+	var self,
+		guideLine = line,
+		_curScale = 1,
+		_assigned = false,
+		_curPosDelta = curDelta || 0,
+		dragContainer = _dragContainer,
+		dimension = lineDimension || ruler.HORIZONTAL,
+		moveCB = moveCB || function(){};
+
+
+	var curPosDelta = function(val){
+		if(typeof val === 'undefined'){
+			return _curPosDelta;
+		}
+		return (_curPosDelta = val);
+	};
+
+	var assigned = function(val){
+		if(typeof val === 'undefined'){
+			return _assigned;
+		}
+		return (_assigned = val);
+	};
+
+	var curScale = function(val){
+		if(typeof val === 'undefined'){
+			return _curScale;
+		}
+		return (_curScale = val);
+	};
+
+
+
+	var draggable = (function(){
+		return {
+			move : function(xpos,ypos){
+				guideLine.style.left = ruler.utils.pixelize(xpos);
+				guideLine.style.top = ruler.utils.pixelize(ypos);
+				updateToolTip(xpos, ypos);
+				moveCB(self, xpos, ypos);
+			},
+			startMoving : function(evt){
+				ruler.utils.addClasss(guideLine, ['rul_line_dragged']);
+				evt = evt || window.event;
+				var posX = evt ? evt.clientX : 0,
+					posY = evt ? evt.clientY : 0,
+					divTop = parseInt(guideLine.style.top || 0),
+					divLeft = parseInt(guideLine.style.left || 0),
+					eWi = parseInt(guideLine.offsetWidth),
+					eHe = parseInt(guideLine.offsetHeight),
+					cWi = parseInt(dragContainer.offsetWidth),
+					cHe = parseInt(dragContainer.offsetHeight),
+					cursor = dimension === ruler.HORIZONTAL ? 'ns-resize' : 'ew-resize';
+
+				options.container.style.cursor=cursor;
+				guideLine.style.cursor=cursor;
+				var diffX = posX - divLeft,
+					diffY = posY - divTop;
+				document.onmousemove =  function moving(evt){
+					evt = evt || window.event;
+					var posX = evt.clientX,
+						posY = evt.clientY,
+						aX = posX - diffX,
+						aY = posY - diffY;
+					if (aX < 0) {
+						aX = 0;
+					}
+					if (aY < 0) {
+						aY = 0;
+					}
+
+					if (aX + eWi > cWi) {
+						aX = cWi - eWi;
+					}
+					if (aY + eHe > cHe) {
+						aY = cHe -eHe;
+					}
+
+					draggable.move(aX,aY);
+				};
+				showToolTip();
+			},
+			stopMoving : function(){
+				options.container.style.cursor=null;
+				guideLine.style.cursor=null;
+				document.onmousemove = function (){};
+				hideToolTip();
+				ruler.utils.removeClasss(guideLine, ['rul_line_dragged']);
+			}
+		}
+	})();
+
+	var showToolTip =  function (e){
+		if(!options.enableToolTip){
+			return;
+		}
+		ruler.utils.addClasss(guideLine, 'rul_tooltip');
+	};
+
+	var updateToolTip = function (x, y){
+		if(y){
+			guideLine.dataset.tip = 'Y: ' + Math.round((y - options.rulerHeight - 1 - _curPosDelta)*_curScale) + ' px';
+		}
+		else{
+			guideLine.dataset.tip = 'X: '  + Math.round((x - options.rulerHeight - 1 - _curPosDelta)*_curScale) + ' px';
+		}
+	};
+
+	var hideToolTip = function (e){
+		ruler.utils.removeClasss(guideLine, 'rul_tooltip');
+	};
+
+	var destroy = function(){
+		draggable.stopMoving();
+		guideLine.parentNode.removeChild(guideLine);
+	};
+
+	var hide = function(){
+		guideLine.style.display = 'none';
+	};
+
+	var show = function(){
+		guideLine.style.display = 'block';
+	};
+
+
+	guideLine.addEventListener('mousedown', function (e){
+		e.stopPropagation();
+		draggable.startMoving();
+	});
+
+	guideLine.addEventListener('mouseup', function (e){
+		draggable.stopMoving();
+	});
+
+	guideLine.addEventListener('dblclick', function (e){
+		e.stopPropagation();
+		destroy();
+	});
+
+	draggable.startMoving(event);
+
+	self = {
+		setAsDraggable: draggable,
+		startDrag: draggable.startMoving,
+		stopDrag:  draggable.stopMoving,
+		destroy: destroy,
+		curScale: curScale,
+		assigned: assigned,
+		curPosDelta: curPosDelta,
+		guideLine: guideLine,
+		dimension: dimension,
+		hide: hide,
+		show: show
+	};
+	return self;
+
+
+
+};
+/**
+ * Created by maor.frankel on 5/25/15.
+ */
+ruler.utils = {
+	extend: function extend(){
+		for(var i=1; i< arguments.length; i++)
+			for(var key in arguments[i])
+				if(arguments[i].hasOwnProperty(key))
+					arguments[0][key] = arguments[i][key];
+		return arguments[0];
+	},
+	pixelize: function (val){
+		return val + 'px';
+	},
+	prependChild: function (container, element){
+		return container.insertBefore(element,container.firstChild);
+	},
+	addClasss: function (element, classNames){
+		if(!(classNames instanceof Array))
+		{
+			classNames = [classNames];
+		}
+
+		classNames.forEach(function (name){
+			element.className += ' ' + name;
+		});
+
+		return element;
+
+	},
+	removeClasss: function (element, classNames){
+		var curCalsss = element.className;
+		if(!(classNames instanceof Array))
+		{
+			classNames = [classNames];
+		}
+
+		classNames.forEach(function (name){
+			curCalsss = curCalsss.replace(name, '');
+		});
+		element.className = curCalsss;
+		return element;
+
+	}
+} ;
